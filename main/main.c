3faa367151f7d6c9fd6354afc5c5bfb24435471d
hanif <hanif314@gmail.com>
2016-03-21 21:58:40 -0400

    refactoring input handling

--- main/main.c
+++ main/main.c
@@ -6,7 +6,7 @@
 #define FALSE 0
 
 int cur_mode;
-
+int running;
 enum LR_MODES
 {
 	LR_DEFAULT,
@@ -80,18 +80,18 @@ void navigate()
 
 
 /*
- *
- * By the time this gets called, mode switching already happened.
- *
- */
+*
+* By the time this gets called, mode switching already happened.
+*
+*/
 void handle_keypress(int key)
 {
 	if((key == LR_L) && (cur_mode == LR_LINKS)){
-		render_links_tab();	
+		render_links_tab();
 	}
 
 	else if((key == LR_J) && (cur_mode == LR_DEFAULT)){
-		shift_down();	
+		shift_down();
 	}
 
 	else if((key == LR_K) && (cur_mode == LR_DEFAULT)){
@@ -124,8 +124,8 @@ void handle_keypress(int key)
 
 	else{
 		SDL_Log("Invalid key combination: %d\n" , key);
-	}		
-}	
+	}
+}
 
 
 // updates the mode from cur_mode to mode or throws an error if the request is invalid
@@ -133,7 +133,7 @@ void update_mode( const int mode )
 {
 	if((cur_mode == LR_DEFAULT) && (mode == LR_LINKS)){
 		cur_mode = LR_LINKS;
-		SDL_Log("Mode Switch from DEFAULT to LINKS\n");		
+		SDL_Log("Mode Switch from DEFAULT to LINKS\n");
 	}
 
 	else if((cur_mode == LR_DEFAULT) && (mode == LR_URL)){
@@ -157,102 +157,111 @@ void update_mode( const int mode )
 	}
 }
 
+void input_controller()
+{
+	SDL_Event event;
+	while( SDL_PollEvent(&event) ){
+		if( event.type == SDL_QUIT){
+			running = FALSE;
+			break;
+		}
+
+		else if( event.type == SDL_KEYDOWN ){
+			switch( event.key.keysym.sym ){
+
+				case SDLK_l:
+				SDL_Log("L key pressed\n");
+				update_mode(LR_LINKS);
+				handle_keypress(LR_L);
+				break;
+				case SDLK_j:
+				SDL_Log("J key pressed\n");
+				handle_keypress(LR_J);
+				break;
+				case SDLK_k:
+				SDL_Log("K key pressed\n");
+				handle_keypress(LR_K);
+				break;
+				case SDLK_f:
+				SDL_Log("F key pressed\n");
+				handle_keypress(LR_F);
+				break;
+				case SDLK_g:
+				SDL_Log("G key pressed\n");
+				handle_keypress(LR_G);
+				break;
+				case SDLK_SPACE:
+				SDL_Log("Space key pressed");
+				update_mode(LR_URL);
+				handle_keypress(LR_SPACE);
+				break;
+				case SDLK_q:
+				SDL_Log("Q key pressed\n");
+				update_mode(LR_DEFAULT);
+				break;
+				case SDLK_ESCAPE:
+				running = FALSE;
+				break;
+
+				case SDLK_UP:
+				SDL_Log("Up key pressed\n");
+				handle_keypress(LR_UP);
+				break;
+				case SDLK_DOWN:
+				SDL_Log("Down key pressed\n");
+				handle_keypress(LR_DOWN);
+				break;
+
+				case SDLK_RETURN:
+				SDL_Log("Return key pressed\n");
+				handle_keypress(LR_RETURN);
+				break;
+			}
+
+		}
+	}
+}
 int main( int argc, char **argv )
 {
 	SDL_Window *window;
+	SDL_Renderer *renderer;
 	int screen_w, screen_h;
-	int running;
 
 	SDL_Init(SDL_INIT_VIDEO);
-	
+
 	SDL_Log( "SDL Initialized\n");
 
 	screen_w = 1024;
 	screen_h = 1024;
-	
+
 	window = SDL_CreateWindow("Key Press Test", SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED, screen_w, screen_h, SDL_WINDOW_OPENGL );
 
 	if( window == NULL ){
 		SDL_LogCritical(SDL_LOG_CATEGORY_ERROR, "Failed to create window: %s\n" , SDL_GetError());
 	}
-	
+
+
+	renderer = SDL_CreateRenderer(window, -1, 0);
+	if( renderer == NULL ){
+		SDL_LogCritical(SDL_LOG_CATEGORY_ERROR, "Failed to create renderer %s\n" , SDL_GetError());
+	}
+
+
 	running = TRUE;
-
-
-	
 	cur_mode = LR_DEFAULT;
 
 	// program main loop
 	while(running){
-		SDL_Event event;
-		while( SDL_PollEvent(&event) ){
-			if( event.type == SDL_QUIT){
-				running = FALSE;
-				break;
-			}
-			
-			else if( event.type == SDL_KEYDOWN ){
-				switch( event.key.keysym.sym ){
-					
-					case SDLK_l:
-						SDL_Log("L key pressed\n");
-						update_mode(LR_LINKS);
-						handle_keypress(LR_L);
-						break;
-					case SDLK_j:
-						SDL_Log("J key pressed\n");					
-						handle_keypress(LR_J);
-						break;
-					case SDLK_k:
-						SDL_Log("K key pressed\n");	
-						handle_keypress(LR_K);
-						break;
-					case SDLK_f:
-						SDL_Log("F key pressed\n");	
-						handle_keypress(LR_F);
-						break;
-					case SDLK_g:
-						SDL_Log("G key pressed\n");
-						handle_keypress(LR_G);
-						break;
-					case SDLK_SPACE:
-						SDL_Log("Space key pressed");	
-						update_mode(LR_URL);
-						handle_keypress(LR_SPACE);
-						break;
-					case SDLK_q:
-						SDL_Log("Q key pressed\n");
-						update_mode(LR_DEFAULT);
-						break;	
-					case SDLK_ESCAPE:
-						running = FALSE;
-						break;
-			
-					case SDLK_UP:
-						SDL_Log("Up key pressed\n");
-						handle_keypress(LR_UP);
-						break;
-					case SDLK_DOWN:
-						SDL_Log("Down key pressed\n");
-						handle_keypress(LR_DOWN);
-						break;
-
-					case SDLK_RETURN:
-						SDL_Log("Return key pressed\n");
-						handle_keypress(LR_RETURN);
-						break;	
-				}	
-			
-			}	
-		}
+		input_controller();
 	}
-	
+
+
 
 
 	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
 	SDL_Quit();
 	printf("Process Complete, exiting \n");
 	return 0;
-	
-}
 
+}
